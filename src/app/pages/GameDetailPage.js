8a1893bd7bde73e1fa272080507a260d4a19f2e4
pgmgent-glendumo <glendumo@student.arteveldehs.be>
2020-11-09 17:56:25 +0100

    added list and detail page

--- src/app/pages/GameDetailPage.js
+++ src/app/pages/GameDetailPage.js
@@ -1,9 +1,81 @@
 import React from "react";
+import { Link, useParams } from "react-router-dom";
+import { gql, useQuery } from "@apollo/client";
+
+import * as Routes from "../routes";
+
+import * as Icon from "react-feather";
+
+import "./GameDetailPage.scss";
 
 const GameDetailPage = ({ children }) => {
+    const { id } = useParams();
+
+    const GET_PRODUCT_DETAIL = gql`
+        query Product($id: ID!) {
+            product(id: $id) {
+                id
+                title
+                description
+                price
+                platform
+                images
+                categories {
+                    id
+                    name
+                }
+                created_on
+            }
+        }
+    `;
+
+    const { loading, error, data } = useQuery(GET_PRODUCT_DETAIL, {
+        variables: { id },
+    });
+
+    if (loading) return "Loading...";
+    if (error) return `Error! ${error.message}`;
+
     return (
-        <div className="page page--game-detail">
-            <h1>Game Detail</h1>
+        <div className={`bg-${data.product.platform}`}>
+            <div className="page page--game-detail">
+                <Link to={Routes.GAMES} className="back-btn">
+                    <Icon.ArrowLeftCircle size={50} />
+                </Link>
+                <div className="container">
+                    <div className="row">
+                        <div className="detail-images col-12 col-md-6">
+                            {data.product.images.map((image) => (
+                                <img src={image} alt={image} key={image} />
+                            ))}
+                        </div>
+                        <div className="detail-content col-12 col-md-6">
+                            <h1>
+                                {data.product.title} - {data.product.platform}
+                            </h1>
+                            <p className="price">€ {data.product.price}</p>
+                            <p>{data.product.description}</p>
+                            <button
+                                type="submit"
+                                className="btn add-to-cart-btn"
+                            >
+                                <Icon.ShoppingCart /> Add to cart
+                            </button>
+                            <ul className="categories-list">
+                                <h3>categories</h3>
+                                {data.product.categories.map((category) => (
+                                    <li
+                                        className="categories-list-item"
+                                        key={category.id}
+                                    >
+                                        {category.name}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/app/pages/GamesPage.js
+++ src/app/pages/GamesPage.js
@@ -4,6 +4,8 @@ import { gql, useQuery } from "@apollo/client";
 
 import * as Routes from "../routes";
 
+import "./GamesPage.scss";
+
 const GamesPage = ({ children }) => {
     const GET_PRODUCTS = gql`
         {
@@ -12,6 +14,7 @@ const GamesPage = ({ children }) => {
                 title
                 price
                 platform
+                images
             }
         }
     `;
@@ -22,18 +25,30 @@ const GamesPage = ({ children }) => {
     if (error) return `Error! ${error.message}`;
 
     return (
-        <div className="container">
-            <div className="products-list row">
-                {data.products.map((product) => (
-                    <Link
-                        to={Routes.GAME_DETAIL.replace(":id", product.id)}
-                        className="col-6 col-md-4"
-                    >
-                        <div className="products-list-item">
-                            <h1>{product.title}</h1>
-                        </div>
-                    </Link>
-                ))}
+        <div className="page page--games">
+            <div className="container">
+                <div className="products-list row">
+                    {data.products.map((product) => (
+                        <Link
+                            to={Routes.GAME_DETAIL.replace(":id", product.id)}
+                            className="col-6 col-md-4 col-lg-3"
+                            key={product.id}
+                        >
+                            <div className="products-list-item">
+                                <img
+                                    src={product.images[0]}
+                                    alt={product.title}
+                                />
+                                <div className="overlay"></div>
+                                <p className="title">{product.title}</p>
+                                <span className="platform">
+                                    {product.platform}
+                                </span>
+                                <span className="price">{product.price}</span>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
             </div>
         </div>
     );
