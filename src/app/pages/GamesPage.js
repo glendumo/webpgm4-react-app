fa16f926048f807f746763c5d032f7b4ed0917bf
pgmgent-glendumo <glendumo@student.arteveldehs.be>
2020-11-09 10:43:11 +0100

    removed warnings

--- src/app/pages/GamesPage.js
+++ src/app/pages/GamesPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { gql, useQuery } from "@apollo/client";
 
 import * as Routes from "../routes";
